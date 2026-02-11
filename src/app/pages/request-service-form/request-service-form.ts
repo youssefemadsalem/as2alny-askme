d0cce6132fb54b0f29e2407e7ee817df1fd865b8
omniafathy1 <omniafathy779@gmail.com>
2026-02-11 11:18:09 +0400

    feat: add content to form page

--- src/app/pages/request-service-form/request-service-form.ts
+++ src/app/pages/request-service-form/request-service-form.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import {  FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-request-service-form',
-  imports: [],
+  imports: [NgIf , ReactiveFormsModule ],
   templateUrl: './request-service-form.html',
   styleUrl: './request-service-form.css',
 })
 export class RequestServiceForm {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      fullName: ['', Validators.required],
+      nationalId: ['', [Validators.required, Validators.pattern(/^\d{14}$/)]],
+      serviceName: ['', Validators.required],
+      description: ['']
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      alert('تم استلام طلبك بنجاح');
+      this.form.reset();
+    }
+  }
 
 }

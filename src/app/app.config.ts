5b2030174b8af4b42562be8122915d4530ba55fc
youssefemadsalem <youssefemadeldin22@gmail.com>
2026-02-15 00:10:43 +0200

    (feat) finish the login logic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withViewTransitions()),
     provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
   ],
 };
---
+++ src/app/core/services/auth/auth.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  x!: any;
+  decode() {
+    if (sessionStorage.getItem('token')) {
+      this.x = jwtDecode(sessionStorage.getItem('token')!);
+      console.log(this.x);
+    }
+  }
+
+  constructor(private _HttpClient: HttpClient) {}
+
+  sighin(data: object): Observable<any> {
+    return this._HttpClient.post(`https://isalny-backend.vercel.app/api/v1/auth/login`, data);
+  }
+}

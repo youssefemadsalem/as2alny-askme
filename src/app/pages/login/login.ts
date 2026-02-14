8031739e5f885278a876d748897b62f0f53d03c6
youssefemadsalem <youssefemadeldin22@gmail.com>
2026-02-15 00:10:43 +0200

    feat(auth) add httpclint ,install jwt-decode , implement login logic

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../core/services/auth/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  constructor(private _Auth: Auth) {}
+  private readonly _Router = inject(Router);
+  alreadyexist!: string;
+  isLoading = signal(false);
+
+  loginform: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+  });
+  login() {
+    this.alreadyexist = '';
+    if (this.loginform.invalid) {
+      this.loginform.markAllAsTouched();
+
+      return;
+    }
+
+    this.isLoading.set(true);
+
+    this._Auth.sighin(this.loginform.value).subscribe({
+      next: (res) => {
+        this.isLoading.set(false);
+
+        if (res.data && res.data.accessToken) {
+          sessionStorage.setItem('token', res.data.accessToken);
+          this._Auth.decode();
+          this._Router.navigate(['/home']);
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        this.alreadyexist = err.error?.message || 'An error occurred';
+        this.isLoading.set(false);
+      },
+    });
+  }
+}

f77cb4dc2472377afce9b9bb535392e599d79319
youssefemadsalem <youssefemadeldin22@gmail.com>
2026-02-28 06:38:12 +0200

    feat(ui): add modern transition effects and fix start position
    - Added modern transition effects to components
    - Fixed issue where transitions always start from the beginning of the component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -10,7 +10,14 @@ import { authInterceptor } from './core/interceptor/refresh-token-interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(
+      routes,
+      withViewTransitions(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+      }),
+    ),
+
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     CookieService,
